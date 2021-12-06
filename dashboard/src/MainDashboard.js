7233972e2c4cd61f9d2f088f5a25c2a49ac33633
Hector Osuna <hosuna@jelp.io>
2021-12-05 18:30:21 -0800

    algo

--- dashboard/src/MainDashboard.js
+++ dashboard/src/MainDashboard.js
@@ -1,5 +1,52 @@
-export const MainDashboard = ({metadata}) => {
+export const MainDashboard = ({ metadata }) => {
+	return (
+		<div>
+			<h3>Taqueros</h3>
+			<div className='justify-content'>
+				{metadata.taqueros.map((taquero) => {
+					return (
+						<div className=''>
+							<p>Taquero: {taquero.name}</p>
+							<p>
+								Tacos hasta descanso:{" "}
+								{taquero["tacos_until_break"]}
+							</p>
+							<div className='queue'>
+								<p>
+									Main Queue:{" "}
+									{taquero.queues["main_queue"].reduce(
+										(prev, curr) => `${prev}, ${curr}`,
+										""
+									)}
+								</p>
+								<p>
+									Priority Queue:{" "}
+									{taquero.queues["priority_queue"].reduce(
+										(prev, curr) => `${prev}, ${curr}`,
+										""
+									)}
+								</p>
 
-return (<div>{JSON.stringify(metadata)}</div>)
-}
-
+								<p>Status: {taquero.status}</p>
+								<hr></hr>
+								<p>Fills</p>
+								<ol>
+									{Object.keys(taquero.fillings).map(
+										(filling) => {
+											return (
+												<li>
+													{filling}:{" "}
+													{taquero.fillings[filling]}
+												</li>
+											);
+										}
+									)}
+								</ol>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
